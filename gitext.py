0ec4c3498b3a9f859ffed199c909dde76487fbb2
Karsten Silkenbäumer <ksi@kluks.de>
2014-02-13 16:40:29 +0100

    Fix import issues due to changes regarding sublime text 3

--- gitext.py
+++ gitext.py
@@ -1,6 +1,6 @@
 import sublime, sublime_plugin
 import Git.git
 
-class GitAddAllCommand(GitTextCommand):
+class GitAddAllCommand(Git.git.GitTextCommand):
     def run(self, edit):
-        self.run_command(['git', 'add', '--all'], working_dir = git_root(self.get_working_dir()))
+        self.run_command(['git', 'add', '--all'], working_dir = Git.git.git_root(self.get_working_dir()))
